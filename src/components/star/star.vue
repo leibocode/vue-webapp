ef013ec3be255f431dcab0df57948b6c70f168a9
leibo <252517584@qq.com>
2019-03-15 12:05:59 +0800

    update

---
+++ src/components/star/star.vue
@@ -0,0 +1,26 @@
+<template>
+    
+</template>
+
+<script>
+const LENGTH =5
+const CLS_ON ='on'
+const CLS_HALF ='half'
+const CLS_OFF ='off'
+
+export default {
+    props:{
+        size:{
+            type:Number
+        },
+        score:{
+            type:Number
+        }
+    },
+    computed:{
+        starType(){
+            
+        }
+    }
+}
+</script>
