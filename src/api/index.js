d43244d80a6b4e890ce0ab02c507e1eacf2f3475
leibo <252517584@qq.com>
2019-04-30 14:34:47 +0800

    add api

--- src/api/index.js
+++ src/api/index.js
@@ -1,1 +1,11 @@
 import { get  } from './http'
+
+const getGoods = get('api/goods')
+const getSeller = get('api/seller')
+const getRatings = get('api/ratings')
+
+export {
+    getSeller,
+    getGoods,
+    getRatings
+}
